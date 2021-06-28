46ad637f98043cb64a381ab6c525c01397ee37e3
RomainGRANGE <rgrange699@gmail.com>
2021-06-28 16:30:03 +0200

    100%

---
+++ Code/index.php
@@ -0,0 +1,34 @@
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="style/style.css">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+</head>
+<body>
+    <div class="Voiture">
+        <div class="Voiture__sedans">
+            <div class="Voiture__General">
+                <img class="Voiture__image" src="../3-column-preview-card-component-main/images/icon-sedans.svg" alt="sedans">
+                <h2 class="Voiture__title">SEDANS</h2>
+                <p class="Voiture__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aliquid velit, ducimus odit error fugit odio vero, nesciunt quisquam eveniet iure at pariatur.</p>
+                <a href="#" class="Voiture__bouton__sedans">Learn More</a>
+            </div>
+        </div>
+        <div class="Voiture__suv">
+            <div class="Voiture__General">
+                <img class="Voiture__image" src="../3-column-preview-card-component-main/images/icon-suvs.svg" alt="suvs">
+                <h2 class="Voiture__title">SUVS</h2>
+                <p class="Voiture__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum qui molestias inventore cupiditate blanditiis soluta nisi perferendis quis cumque nobis, dolorum eveniet quia?</p>
+                <a href="#" class="Voiture__bouton__suv">Learn More</a>
+            </div>
+        </div>
+        <div class="Voiture__luxe">
+            <div class="Voiture__General">
+                <img class="Voiture__image" src="../3-column-preview-card-component-main/images/icon-luxury.svg" alt="luxury">
+                <h2 class="Voiture__title">LUXURY</h2>
+                <p class="Voiture__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio corporis soluta impedit nihil ipsa perspiciatis consequatur laborum, assumenda itaque atque sapiente odit veritatis!</p>
+                <a href="#" class="Voiture__bouton__luxe">Learn More</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
